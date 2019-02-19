99321d2d46e737c36b5c86031f8612e37249523a
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-02-19 12:44:09 +1100

    Changed uom service and ingredient controller to reactive

--- src/main/java/guru/springframework/services/UnitOfMeasureService.java
+++ src/main/java/guru/springframework/services/UnitOfMeasureService.java
@@ -1,13 +1,14 @@
 package guru.springframework.services;
 
 import guru.springframework.commands.UnitOfMeasureCommand;
-
-import java.util.Set;
+import guru.springframework.domain.UnitOfMeasure;
+import reactor.core.publisher.Flux;
 
 /**
  * Created by jt on 6/28/17.
  */
 public interface UnitOfMeasureService {
 
-    Set<UnitOfMeasureCommand> listAllUoms();
+    //Set<UnitOfMeasureCommand> listAllUoms();
+    Flux<UnitOfMeasureCommand> listAllUoms();
 }
--- src/main/java/guru/springframework/services/UnitOfMeasureServiceImpl.java
+++ src/main/java/guru/springframework/services/UnitOfMeasureServiceImpl.java
@@ -1,13 +1,11 @@
 package guru.springframework.services;
 
-import guru.springframework.commands.UnitOfMeasureCommand;
-import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
-import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import guru.springframework.commands.UnitOfMeasureCommand;
+import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import guru.springframework.repositories.reactive.UnitOfMeasureReactiveRepository;
+import reactor.core.publisher.Flux;
 
 /**
  * Created by jt on 6/28/17.
@@ -15,20 +13,18 @@ import java.util.stream.StreamSupport;
 @Service
 public class UnitOfMeasureServiceImpl implements UnitOfMeasureService {
 
-    private final UnitOfMeasureRepository unitOfMeasureRepository;
-    private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
+	private final UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
+	private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
 
-    public UnitOfMeasureServiceImpl(UnitOfMeasureRepository unitOfMeasureRepository, UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
-        this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureToUnitOfMeasureCommand;
-    }
+	public UnitOfMeasureServiceImpl(UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository,
+			UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
+		this.unitOfMeasureReactiveRepository = unitOfMeasureReactiveRepository;
+		this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureToUnitOfMeasureCommand;
+	}
 
-    @Override
-    public Set<UnitOfMeasureCommand> listAllUoms() {
+	@Override
+	public Flux<UnitOfMeasureCommand> listAllUoms() {
 
-        return StreamSupport.stream(unitOfMeasureRepository.findAll()
-                .spliterator(), false)
-                .map(unitOfMeasureToUnitOfMeasureCommand::convert)
-                .collect(Collectors.toSet());
-    }
+		return unitOfMeasureReactiveRepository.findAll().map(unitOfMeasureToUnitOfMeasureCommand::convert);
+	}
 }
